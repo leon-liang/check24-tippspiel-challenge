13e082ee08bdad6f5f305c42f45a9890001828ce
Leon Liang <hello@leonliang.lu>
2024-04-13 22:11:28 +0200

    add .env file on server

--- server/db/db.go
+++ server/db/db.go
@@ -5,11 +5,20 @@ import (
 	"gorm.io/driver/postgres"
 	_ "gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func New() *gorm.DB {
-	dbUrl := "postgresql://db:5432/master?user=root&password=password"
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println("Database Error: ", err)
