d2c9f6c6a70719ed02e065f7aad6074aa4841d9d
Leon Liang <hello@leonliang.lu>
2024-04-21 01:55:04 +0200

    add animation when collapsing / expanding nested sidebar items

--- client/src/components/sidebar/SidebarItem.tsx
+++ client/src/components/sidebar/SidebarItem.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import ChevronUp from "@/components/icons/ChevronUp";
-import ChevronDown from "@/components/icons/ChevronDown";
+import { motion, AnimatePresence } from "framer-motion";
 
 export interface SidebarItemProps {
   icon?: React.ReactNode;
@@ -26,22 +26,37 @@ const SidebarItem = ({ icon, label, nestedItems }: SidebarItemProps) => {
           <span>{icon ?? null}</span>
           <p>{label}</p>
         </div>
-        {nestedItems &&
-          (showNestedItems ? (
+        {nestedItems && (
+          <motion.div
+            animate={{ rotate: showNestedItems ? 0 : -180 }}
+            transition={{ ease: "linear", duration: 0.1 }}
+          >
             <ChevronUp width={28} height={28} />
-          ) : (
-            <ChevronDown width={28} height={28} />
-          ))}
+          </motion.div>
+        )}
       </div>
       <div className="ml-4">
-        {showNestedItems &&
-          nestedItems?.map((nestedItemProps, index) => {
-            return (
-              <div key={index} className="border-l-2 border-gray-6">
-                <SidebarItem {...nestedItemProps} />
-              </div>
-            );
-          })}
+        <AnimatePresence>
+          {showNestedItems &&
+            nestedItems?.map((nestedItemProps, index) => {
+              return (
+                <motion.div
+                  key={index}
+                  className="border-l-2 border-gray-6"
+                  initial="collapsed"
+                  animate="open"
+                  exit="collapsed"
+                  variants={{
+                    open: { opacity: 1, height: "auto" },
+                    collapsed: { opacity: 0, height: 0 },
+                  }}
+                  transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                >
+                  <SidebarItem {...nestedItemProps} />
+                </motion.div>
+              );
+            })}
+        </AnimatePresence>
       </div>
     </div>
   );
