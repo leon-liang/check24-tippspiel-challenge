684888acd465dc0cfed265381cfecf4e7c4077da
Leon Liang <hello@leonliang.lu>
2024-04-24 14:23:42 +0200

    create JoinCommunityDialog

--- client/src/components/join-community-dialog/JoinCommunityDialog.tsx
+++ client/src/components/join-community-dialog/JoinCommunityDialog.tsx
@@ -1,9 +1,3 @@
-import React from "react";
-import { useCreateCommunity } from "@/hooks/communities.api";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useRouter } from "next/navigation";
 import {
   Dialog,
   DialogContent,
@@ -12,55 +6,50 @@ import {
   DialogTrigger,
 } from "@/components/dialog/Dialog";
 import Button from "@/components/button/Button";
+import { SubmitHandler, useForm } from "react-hook-form";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-const CreateCommunityDialog = () => {
-  const mutation = useCreateCommunity();
-  const [open, setOpen] = React.useState(false);
-  const router = useRouter();
+const JoinCommunityDialog = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
 
   const FormSchema = z.object({
-    communityName: z.string().min(1),
+    communityTag: z.string().min(36),
   });
 
   type FormData = z.infer<typeof FormSchema>;
-
   const { register, handleSubmit } = useForm<FormData>({
     resolver: zodResolver(FormSchema),
   });
 
   const onSubmit: SubmitHandler<FormData> = async (data, event) => {
     event?.target.reset();
-    const response = await mutation.mutateAsync({
-      community: {
-        name: data.communityName,
-      },
-    });
     setOpen(false);
-    router.push(`/communities/${response.data.community?.id}`);
   };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button>Create Community</Button>
+        <Button>Join Community</Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogTitle>Create Community</DialogTitle>
+        <DialogTitle>Join a Community</DialogTitle>
         <DialogDescription>
-          Give your community a name. Click save when you are done.
+          Enter the community tag below. Click join when you are done.
         </DialogDescription>
         <form onSubmit={handleSubmit(onSubmit)}>
           <fieldset className="mb-[15px] flex items-center gap-5">
             <label
               className="w-[90px] text-right text-[15px] text-indigo-11"
-              htmlFor="community-name"
+              htmlFor="community-tag"
             >
-              Community Name
+              Community Tag
             </label>
             <input
               className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-indigo-11 shadow-[0_0_0_1px] shadow-indigo-7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-indigo-8"
-              {...register("communityName")}
-              id="community-name"
+              {...register("communityTag")}
+              id="community-tag"
             />
           </fieldset>
           <div className="mt-[25px] flex justify-end">
@@ -68,7 +57,7 @@ const CreateCommunityDialog = () => {
               type="submit"
               className="inline-flex h-[35px] w-[80px] items-center justify-center rounded-[4px] bg-colors-gray-12 px-[15px] leading-none text-white-A12 hover:bg-colors-gray-11 focus:shadow-[0_0_0_2px] focus:shadow-gray-7 focus:outline-none"
             >
-              Save
+              Join
             </button>
           </div>
         </form>
@@ -77,4 +66,4 @@ const CreateCommunityDialog = () => {
   );
 };
 
-export default CreateCommunityDialog;
+export default JoinCommunityDialog;
