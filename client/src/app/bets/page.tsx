32f210b1d9fce02d6cdaca0245bee237009c5f3a
Leon Liang <hello@leonliang.lu>
2024-05-11 11:28:24 +0200

    useMemo and translated dataset to english

--- client/src/app/bets/page.tsx
+++ client/src/app/bets/page.tsx
@@ -77,36 +77,38 @@ const Bets = () => {
     [],
   );
 
+  const matches = useMemo(() => {
+    return data?.data.bets?.map((bet) => {
+      const date = DateTime.fromISO(
+        bet.match?.match?.gameTime?.slice(0, -1) ?? "",
+      );
+
+      const currentRound = rounds.find((round) => round.dates.contains(date));
+
+      return {
+        homeTeam: {
+          name: bet.match?.match?.homeTeam?.name ?? "",
+          bet: bet.homeTeam,
+          result: bet.match?.match?.homeTeam?.result,
+        },
+        awayTeam: {
+          name: bet.match?.match?.awayTeam?.name ?? "",
+          bet: bet.awayTeam,
+          result: bet.match?.match?.awayTeam?.result,
+        },
+        round: currentRound?.name as Round,
+        date: date,
+      };
+    });
+  }, [data?.data.bets, rounds]);
+
   useEffect(() => {
     const currentDate = DateTime.now();
     const currentRound = rounds.find((round) =>
       round.dates.contains(currentDate),
     );
     setSelectedRound(currentRound ? currentRound.name : "Match day 1");
-  }, []);
-
-  const matches = data?.data.bets?.map((bet) => {
-    const date = DateTime.fromISO(
-      bet.match?.match?.gameTime?.slice(0, -1) ?? "",
-    );
-
-    const currentRound = rounds.find((round) => round.dates.contains(date));
-
-    return {
-      homeTeam: {
-        name: bet.match?.match?.homeTeam?.name ?? "",
-        bet: bet.homeTeam,
-        result: bet.match?.match?.homeTeam?.result,
-      },
-      awayTeam: {
-        name: bet.match?.match?.awayTeam?.name ?? "",
-        bet: bet.awayTeam,
-        result: bet.match?.match?.awayTeam?.result,
-      },
-      round: currentRound?.name as Round,
-      date: date,
-    };
-  });
+  }, [rounds]);
 
   return (
     <div>
