8f1bd8aa23e5a3be85c7d2206e01d2444e24e904
Leon Liang <hello@leonliang.lu>
2024-05-20 16:46:04 +0200

    send message received from kafka over socket

--- server/handler/ws/match.go
+++ server/handler/ws/match.go
@@ -19,20 +19,19 @@ func (h *Handler) wsMatches(ctx echo.Context) error {
 	reader.SetOffset(-1)
 	defer reader.Close()
 
-	//ws.SetCloseHandler(func(code int, text string) error {
-	//	reader.Close()
-	//	fmt.Println("Received connection close request. Closing connection...")
-	//	return nil
-	//})
+	ws.SetCloseHandler(func(code int, text string) error {
+		reader.Close()
+		fmt.Println("Received connection close request. Closing connection...")
+		return nil
+	})
 
 	for {
 		message, err := reader.ReadMessage(context.Background())
 		if err != nil {
 			fmt.Println("Error: ", err)
 		}
-		fmt.Println("Reading", string(message.Value))
 
-		if err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!")); err != nil {
+		if err := ws.WriteMessage(websocket.TextMessage, message.Value); err != nil {
 			fmt.Println(err)
 		}
 	}
