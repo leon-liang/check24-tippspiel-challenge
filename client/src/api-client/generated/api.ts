f7cc55403d8087cd047240ea87ce73ea467e7dce
Leon Liang <hello@leonliang.lu>
2024-05-21 17:37:19 +0200

    generate-client

--- client/src/api-client/generated/api.ts
+++ client/src/api-client/generated/api.ts
@@ -1432,6 +1432,175 @@ export class CommunitiesApi extends BaseAPI {
 }
 
 /**
+ * CommunityApi - axios parameter creator
+ * @export
+ */
+export const CommunityApiAxiosParamCreator = function (
+  configuration?: Configuration,
+) {
+  return {
+    /**
+     *
+     * @summary Delete the specified community
+     * @param {string} communityId Community ID
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    v1CommunitiesCommunityIdDelete: async (
+      communityId: string,
+      options: RawAxiosRequestConfig = {},
+    ): Promise<RequestArgs> => {
+      // verify required parameter 'communityId' is not null or undefined
+      assertParamExists(
+        "v1CommunitiesCommunityIdDelete",
+        "communityId",
+        communityId,
+      );
+      const localVarPath = `/v1/communities/{community_id}`.replace(
+        `{${"community_id"}}`,
+        encodeURIComponent(String(communityId)),
+      );
+      // use dummy base URL string because the URL constructor only accepts absolute URLs.
+      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+      let baseOptions;
+      if (configuration) {
+        baseOptions = configuration.baseOptions;
+      }
+
+      const localVarRequestOptions = {
+        method: "DELETE",
+        ...baseOptions,
+        ...options,
+      };
+      const localVarHeaderParameter = {} as any;
+      const localVarQueryParameter = {} as any;
+
+      // authentication OAuth2Implicit required
+      // oauth required
+      await setOAuthToObject(
+        localVarHeaderParameter,
+        "OAuth2Implicit",
+        [],
+        configuration,
+      );
+
+      setSearchParams(localVarUrlObj, localVarQueryParameter);
+      let headersFromBaseOptions =
+        baseOptions && baseOptions.headers ? baseOptions.headers : {};
+      localVarRequestOptions.headers = {
+        ...localVarHeaderParameter,
+        ...headersFromBaseOptions,
+        ...options.headers,
+      };
+
+      return {
+        url: toPathString(localVarUrlObj),
+        options: localVarRequestOptions,
+      };
+    },
+  };
+};
+
+/**
+ * CommunityApi - functional programming interface
+ * @export
+ */
+export const CommunityApiFp = function (configuration?: Configuration) {
+  const localVarAxiosParamCreator =
+    CommunityApiAxiosParamCreator(configuration);
+  return {
+    /**
+     *
+     * @summary Delete the specified community
+     * @param {string} communityId Community ID
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async v1CommunitiesCommunityIdDelete(
+      communityId: string,
+      options?: RawAxiosRequestConfig,
+    ): Promise<
+      (
+        axios?: AxiosInstance,
+        basePath?: string,
+      ) => AxiosPromise<HttpCommunityResponse>
+    > {
+      const localVarAxiosArgs =
+        await localVarAxiosParamCreator.v1CommunitiesCommunityIdDelete(
+          communityId,
+          options,
+        );
+      const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+      const localVarOperationServerBasePath =
+        operationServerMap["CommunityApi.v1CommunitiesCommunityIdDelete"]?.[
+          localVarOperationServerIndex
+        ]?.url;
+      return (axios, basePath) =>
+        createRequestFunction(
+          localVarAxiosArgs,
+          globalAxios,
+          BASE_PATH,
+          configuration,
+        )(axios, localVarOperationServerBasePath || basePath);
+    },
+  };
+};
+
+/**
+ * CommunityApi - factory interface
+ * @export
+ */
+export const CommunityApiFactory = function (
+  configuration?: Configuration,
+  basePath?: string,
+  axios?: AxiosInstance,
+) {
+  const localVarFp = CommunityApiFp(configuration);
+  return {
+    /**
+     *
+     * @summary Delete the specified community
+     * @param {string} communityId Community ID
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    v1CommunitiesCommunityIdDelete(
+      communityId: string,
+      options?: any,
+    ): AxiosPromise<HttpCommunityResponse> {
+      return localVarFp
+        .v1CommunitiesCommunityIdDelete(communityId, options)
+        .then((request) => request(axios, basePath));
+    },
+  };
+};
+
+/**
+ * CommunityApi - object-oriented interface
+ * @export
+ * @class CommunityApi
+ * @extends {BaseAPI}
+ */
+export class CommunityApi extends BaseAPI {
+  /**
+   *
+   * @summary Delete the specified community
+   * @param {string} communityId Community ID
+   * @param {*} [options] Override http request option.
+   * @throws {RequiredError}
+   * @memberof CommunityApi
+   */
+  public v1CommunitiesCommunityIdDelete(
+    communityId: string,
+    options?: RawAxiosRequestConfig,
+  ) {
+    return CommunityApiFp(this.configuration)
+      .v1CommunitiesCommunityIdDelete(communityId, options)
+      .then((request) => request(this.axios, this.basePath));
+  }
+}
+
+/**
  * MatchesApi - axios parameter creator
  * @export
  */
