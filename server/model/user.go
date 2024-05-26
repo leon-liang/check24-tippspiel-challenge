c2cef6c099f0aab784a589cde861dbb58861d9f6
Leon Liang <hello@leonliang.lu>
2024-05-26 13:46:20 +0200

    add [PUT] and [DELETE] /communities/{community_id}/pinned_users/{user_id}

--- server/model/user.go
+++ server/model/user.go
@@ -20,6 +20,7 @@ type User struct {
 }
 
 type UserCommunity struct {
+	gorm.Model
 	UserID      string `gorm:"primaryKey"`
 	CommunityID string `gorm:"primaryKey;constraint:OnDelete:CASCADE;"`
 	PinnedUsers []User `gorm:"many2many:user_community_pinned_users;"`
