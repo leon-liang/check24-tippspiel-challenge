7bfe00751d90a3876954c8c7068e7864ba620a20
Leon Liang <hello@leonliang.lu>
2024-05-27 23:10:38 +0200

    work in progress

--- server/store/community.go
+++ server/store/community.go
@@ -85,12 +85,12 @@ func (cs *CommunityStore) IsMember(user *model.User, community *model.Community)
 	query := `
 		SELECT EXISTS (
 			SELECT 1
-			FROM community_members
-			WHERE community_id = ? AND user_id = ?
+			FROM community_members, communities
+			WHERE community_id = ? AND user_id = ? OR communities.owner = ?
 		)
 	`
 
-	if err := cs.db.Raw(query, community.ID, user.ID).Scan(&isMember).Error; err != nil {
+	if err := cs.db.Raw(query, community.ID, user.ID, user.ID).Scan(&isMember).Error; err != nil {
 		return false, err
 	}
 
